a1355704bb7524224167da14f5f589eade44c0b9
protectandserve <xmaeyuri@gmail.com>
2026-05-12 18:41:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
 window.addEventListener("load", () => {
+
+  const entries = document.querySelectorAll(".boot-entry");
+
+  entries.forEach((entry, index) => {
+    setTimeout(() => {
+      entry.classList.add("visible");
+    }, 700 + (index * 650));
+  });
+
   setTimeout(() => {
     document.getElementById("boot").classList.add("hidden");
-  }, 2450);
+  }, 6200);
 });
 
 const observer = new IntersectionObserver((entries) => {
